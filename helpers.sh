b64f15d034e9c53a5e1b59f701614dfe3c73ebf0
flare576 <flare576@gmail.com>
2025-04-24 12:17:54 -0500

    feat(scripts): dryRun, helpers, meta redirect

---
+++ helpers.sh
@@ -0,0 +1,14 @@
+
+# Takes a MD file & field, returns un-quoted value
+get_meta() {
+  local md_file="$1"
+  local field="$2"
+  local awk_string=$(printf '/^---/ { flag = !flag; next }
+  flag && /^%s:/ {
+    val = substr($0, index($0, $2));
+    gsub(/^["'\'']|["'\'']$/, "", val);
+    print val
+  }
+  ' "$field")
+  awk "$awk_string" "$md_file"
+}
